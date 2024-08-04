c270368f5554f442bcdb2af042a6a7831e488f72
maxboyko <maxboiko@divotek.com>
2024-08-04 13:09:09 +0300

    fix for cache

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,11 @@ use App\Models\SystemPage;
 use Illuminate\Support\Facades\Route;
 use Modules\News\Controllers\NewsController;
 
-function news_slug()
-{
-   $page = StaticPage::query()->where('id', SystemPage::query()->where('name', 'News')->first()->page_id ?? 0)->first();
-   return $page && $page->slug ? $page->slug : 'news';
+if (!function_exists('news_slug')) {
+   function news_slug()
+   {
+      $page = StaticPage::query()->where('id', SystemPage::query()->where('name', 'News')->first()->page_id ?? 0)->first();
+      return $page && $page->slug ? $page->slug : 'news';
+   }
 }
 Route::get(news_slug() . '/{post}', [NewsController::class, 'post'])->name('news-post');
